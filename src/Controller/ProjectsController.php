f467888bd58cee75966f23e7b6bbeb393f2514e0
nico <ngretten@gmail.com>
2020-03-28 18:47:27 +0100

    page projets

--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -13,7 +13,83 @@ class ProjectsController extends AbstractController
     public function index()
     {
         return $this->render('projects/index.html.twig', [
-            'controller_name' => 'ProjectsController',
+            'controller_name' => 'Projects',
+            'sites' => [ 
+                [
+                    'index' => 1,
+                    'image' =>'/img/screen_wordpress.png',
+                    'gif' =>'/img/screen_wordpress.png',
+                    'name' =>'Wordpress',
+                    'desc' =>'Voici un exemple d\'installation Wordpress que j\'ai effectué avec des plugins personnalisez !',
+                    'icon' =>'fab fa-wordpress',
+                    'link' =>'http://nicolas-gretten.fr:8081'
+                ],
+                [
+                    'index' => 2,
+                    'image' =>'/img/screen_boutique.png',
+                    'gif' => '/img/screen_boutique.png',
+                    'name' =>'E-boutique',
+                    'desc' =>'Je vous présente la boutique réaliser lors de ma formation de développeur web et web mobile.',
+                    'icon' =>'fas fa-shopping-cart',
+                    'link' =>'http://nicolas-gretten.fr:8082'
+                ],
+                [
+                    'index' => 3,
+                    'image' => '/img/screen.png',
+                    'gif' => '/img/avoizejotatttoo.gif',
+                    'name' =>'Avoizejotattoo.lu',
+                    'desc' =>'Lors de mon stage j\'ai eu la chance de pouvoir réaliser le site internet pour le salon de tatouage Avoize & Jo Tatto Studio',
+                    'icon' =>'fas fa-code',
+                    'link' =>'https://avoizejotattoo.lu'
+                ],
+            ],
+            'games' => [ 
+                [
+                    'image' =>'https://dummyimage.com/600x400/28d6a5/fff',
+                    'name' =>'Mastermind',
+                    'desc' =>'Mastermind en JavaScript',
+                    'icon' =>'fas fa-dice-one',
+                    'link' =>'#'
+                ],
+                [
+                    'image' =>'https://dummyimage.com/600x400/28d6a5/fff',
+                    'name' =>'Morpion',
+                    'desc' =>'Morpion en JavaScript',
+                    'icon' =>'fas fa-dice-two',
+                    'link' =>'#'
+                ],
+                [
+                    'image' =>'https://dummyimage.com/600x400/28d6a5/fff',
+                    'name' =>'To Do List',
+                    'desc' =>'To do list en JavaScript',
+                    'icon' =>'fas fa-dice-three',
+                    'link' =>'#'
+                ],
+            ],
+            'designs' => [ 
+                [
+                    'image' =>'https://dummyimage.com/600x400/2685eb/fff',
+                    'name' =>'Leduc guitar',
+                    'desc' =>'Premier maquettage sur InDesign, maquettage d\'une application de gestion de commandes pour un luthier.',
+                    'icon' =>'fas fa-palette',
+                    'link' =>'#'
+                ],
+                [
+                    'image' =>'https://dummyimage.com/600x400/2685eb/fff',
+                    'name' =>'avoizejotattoo.lu',
+                    'desc' =>'Maquettage du Front End pour le site avoizejotattoo.lu avec dialogue écrans',
+                    'icon' =>'fas fa-palette',
+                    'link' =>'#'
+                ],
+                [
+                    'image' =>'https://dummyimage.com/600x400/2685eb/fff',
+                    'name' =>'avoizejotattoo.lu',
+                    'desc' =>'Maquettage du Panel admin pour le site avoizejotattoo.lu avec dialogue écrans',
+                    'icon' =>'fas fa-palette',
+                    'link' =>'#'
+                ],
+            ],
+
         ]);
     }
 }
